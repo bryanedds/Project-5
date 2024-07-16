b269688244d5288278442fae338e6925389b6bef
bryanedds <bryanedds@gmail.com>
2024-07-15 20:29:29 -0400

    Added initial Project 5 files.

---
+++ Projects/Project 5/Character.fs
@@ -0,0 +1,155 @@
+﻿namespace Project5
+open System
+open System.Numerics
+open Prime
+open Nu
+
+[<AutoOpen>]
+module CharacterDispatcher =
+
+    type CharacterModel =
+        { CharacterTime : int64
+          LastTimeJump : int64
+          LastTimeOnGround : int64
+          AnimatedModel : AnimatedModel AssetTag }
+
+        static member initial =
+            { CharacterTime = 0L
+              LastTimeJump = 0L
+              LastTimeOnGround = 0L
+              AnimatedModel = Assets.Default.AnimatedModel }
+
+    type CharacterMessage =
+        | UpdateMessage
+        | TryJump of KeyboardKeyData
+        interface Message
+
+    type CharacterCommand =
+        | UpdateCommand
+        | PostUpdate
+        | Jump
+        interface Command
+
+    type CharacterDispatcher () =
+        inherit Entity3dDispatcher<CharacterModel, CharacterMessage, CharacterCommand> (true, false, false, CharacterModel.initial)
+
+        static let [<Literal>] WalkForce = 3.5f
+        static let [<Literal>] TurnForce = 8.0f
+        static let [<Literal>] JumpForce = 4.0f
+
+        static member Facets =
+            [typeof<AnimatedModelFacet>
+             typeof<RigidBodyFacet>]
+
+        override this.Definitions (character, _) =
+            [Entity.MaterialProperties == MaterialProperties.defaultProperties
+             Entity.AnimatedModel := character.AnimatedModel
+             Entity.Friction == 1.0f
+             Entity.LinearDamping == 0.5f
+             Entity.AngularDamping == 0.999f
+             Entity.AngularFactor == v3 0.0f 0.1f 0.0f
+             Entity.BodyShape == CapsuleShape { Height = 1.0f; Radius = 0.35f; TransformOpt = Some (Affine.makeTranslation (v3 0.0f 0.85f 0.0f)); PropertiesOpt = None }
+             Entity.UpdateEvent => UpdateMessage
+             Entity.UpdateEvent => UpdateCommand
+             Game.PostUpdateEvent => PostUpdate
+             Game.KeyboardKeyDownEvent =|> fun evt -> TryJump evt.Data]
+
+        override this.Message (character, message, entity, world) =
+
+            match message with
+            | UpdateMessage ->
+                let time = inc character.CharacterTime
+                let bodyId = entity.GetBodyId world
+                let grounded = World.getBodyGrounded bodyId world
+                let character =
+                    { character with
+                        CharacterTime = time
+                        LastTimeOnGround = if grounded then time else character.LastTimeOnGround }
+                just character
+
+            | TryJump keyboardKeyData ->
+                let sinceJump = character.CharacterTime - character.LastTimeJump
+                let sinceOnGround = character.CharacterTime - character.LastTimeOnGround
+                if keyboardKeyData.KeyboardKey = KeyboardKey.Space && not keyboardKeyData.Repeated && sinceJump >= 12L && sinceOnGround < 10L then
+                    let character = { character with LastTimeJump = character.CharacterTime }
+                    withSignal Jump character
+                else just character
+
+        override this.Command (_, command, entity, world) =
+
+            match command with
+            | UpdateCommand ->
+
+                // apply physics-based animations
+                let bodyId = entity.GetBodyId world
+                let grounded = World.getBodyGrounded bodyId world
+                let rotation = entity.GetRotation world
+                let linearVelocity = World.getBodyLinearVelocity bodyId world
+                let angularVelocity = World.getBodyAngularVelocity bodyId world
+                let forwardness = (Vector3.Dot (linearVelocity, rotation.Forward))
+                let backness = (Vector3.Dot (linearVelocity, -rotation.Forward))
+                let rightness = (Vector3.Dot (linearVelocity, rotation.Right))
+                let leftness = (Vector3.Dot (linearVelocity, -rotation.Right))
+                let turnRightness = (angularVelocity * v3Up).Length ()
+                let turnLeftness = -turnRightness
+                let animations = [{ StartTime = 0L; LifeTimeOpt = None; Name = "Armature|Idle"; Playback = Loop; Rate = 1.0f; Weight = 0.5f; BoneFilterOpt = None }]
+                let animations =
+                    if forwardness >= 0.1f then { StartTime = 0L; LifeTimeOpt = None; Name = "Armature|WalkForward"; Playback = Loop; Rate = 1.0f; Weight = forwardness; BoneFilterOpt = None } :: animations
+                    elif backness >= 0.1f then { StartTime = 0L; LifeTimeOpt = None; Name = "Armature|WalkBack"; Playback = Loop; Rate = 1.0f; Weight = backness; BoneFilterOpt = None } :: animations
+                    else animations
+                let animations =
+                    if rightness >= 0.1f then { StartTime = 0L; LifeTimeOpt = None; Name = "Armature|WalkRight"; Playback = Loop; Rate = 1.0f; Weight = rightness; BoneFilterOpt = None } :: animations
+                    elif leftness >= 0.1f then { StartTime = 0L; LifeTimeOpt = None; Name = "Armature|WalkLeft"; Playback = Loop; Rate = 1.0f; Weight = leftness; BoneFilterOpt = None } :: animations
+                    else animations
+                let animations =
+                    if turnRightness >= 0.1f then { StartTime = 0L; LifeTimeOpt = None; Name = "Armature|TurnRight"; Playback = Loop; Rate = 1.0f; Weight = turnRightness; BoneFilterOpt = None } :: animations
+                    elif turnLeftness >= 0.1f then { StartTime = 0L; LifeTimeOpt = None; Name = "Armature|TurnLeft"; Playback = Loop; Rate = 1.0f; Weight = turnLeftness; BoneFilterOpt = None } :: animations
+                    else animations
+                let world = entity.SetAnimations (List.toArray animations) world
+
+                // apply walk force
+                let forward = rotation.Forward
+                let right = rotation.Right
+                let contactNormalOpt = World.getBodyToGroundContactNormalOpt bodyId world
+                let walkForceScalar = if grounded then WalkForce else WalkForce * 0.5f
+                let walkForce = 
+                    (if World.isKeyboardKeyDown KeyboardKey.W world || World.isKeyboardKeyDown KeyboardKey.Up world then forward * walkForceScalar else v3Zero) +
+                    (if World.isKeyboardKeyDown KeyboardKey.S world || World.isKeyboardKeyDown KeyboardKey.Down world then -forward * walkForceScalar else v3Zero) +
+                    (if World.isKeyboardKeyDown KeyboardKey.A world then -right * walkForceScalar else v3Zero) +
+                    (if World.isKeyboardKeyDown KeyboardKey.D world then right * walkForceScalar else v3Zero)
+                let world =
+                    if walkForce <> v3Zero then
+                        match contactNormalOpt with
+                        | Some contactNormal ->
+                            let walkForward = walkForce.Normalized
+                            let groundPlane = Plane3 (contactNormal, -1.0f)
+                            let slope = Vector3.Project (walkForward, groundPlane) - v3Up
+                            let angle = Vector3.Dot (slope, v3Up)
+                            let walkForceOriented =
+                                if abs angle > 0.001f then // guard against generating NaNs
+                                    let angleBetween = walkForward.AngleBetween slope
+                                    let rotationMatrix = Matrix4x4.CreateFromAxisAngle (Vector3.Cross (walkForward, v3Up), angleBetween)
+                                    let walkForceOriented = Vector3.Transform (walkForce, rotationMatrix)
+                                    walkForceOriented
+                                else walkForce
+                            World.applyBodyForce walkForceOriented v3Zero bodyId world
+                        | None -> World.applyBodyForce walkForce v3Zero bodyId world
+                    else world
+
+                // apply turn force
+                let turnForce = if grounded then TurnForce else TurnForce * 0.5f
+                let world = if World.isKeyboardKeyDown KeyboardKey.Right world then World.applyBodyTorque (-v3Up * turnForce) bodyId world else world
+                let world = if World.isKeyboardKeyDown KeyboardKey.Left world then World.applyBodyTorque (v3Up * turnForce) bodyId world else world
+                just world
+
+            | PostUpdate ->
+                let rotation = entity.GetRotation world
+                let position = entity.GetPosition world
+                let world = World.setEye3dRotation rotation world
+                let world = World.setEye3dCenter (position + v3Up * 1.45f - rotation.Forward * 1.0f + rotation.Right * 0.35f) world
+                just world
+
+            | Jump ->
+                let bodyId = entity.GetBodyId world
+                let world = World.applyBodyLinearImpulse (v3Up * JumpForce) v3Zero bodyId world
+                just world
---
+++ Projects/Project 5/Project5.fs
@@ -0,0 +1,75 @@
+﻿namespace Project5
+open System
+open System.Numerics
+open Prime
+open Nu
+
+// this is our top-level MMCC model type. It determines what state the game is in. To learn about MMCC in Nu, see -
+// https://github.com/bryanedds/Nu/wiki/Model-View-Update-for-Games-via-MMCC
+type Project5 =
+    | Splash
+    | Title
+    | Credits
+    | Gameplay
+
+// this is our top-level MMCC message type.
+type Project5Message =
+    | ShowTitle
+    | ShowCredits
+    | ShowGameplay
+    interface Message
+
+// this is our top-level MMCC command type. Commands are used instead of messages when the world is to be transformed.
+type Project5Command =
+    | Exit
+    interface Command
+
+// this extends the Game API to expose the above MMCC model as a property.
+[<AutoOpen>]
+module Project5Extensions =
+    type Game with
+        member this.GetProject5 world = this.GetModelGeneric<Project5> world
+        member this.SetProject5 value world = this.SetModelGeneric<Project5> value world
+        member this.Project5 = this.ModelGeneric<Project5> ()
+
+// this is the dispatcher that customizes the top-level behavior of our game. In here, we create screens as content and
+// bind them up with events and properties.
+type Project5Dispatcher () =
+    inherit GameDispatcher<Project5, Project5Message, Project5Command> (Splash)
+
+    // here we define the game's properties and event handling
+    override this.Definitions (project5, _) =
+        [Game.DesiredScreen :=
+            match project5 with
+            | Splash -> Desire Simulants.Splash
+            | Title -> Desire Simulants.Title
+            | Credits -> Desire Simulants.Credits
+            | Gameplay -> Desire Simulants.Gameplay
+            if project5 = Splash then Simulants.Splash.DeselectingEvent => ShowTitle
+            Simulants.TitleCredits.ClickEvent => ShowCredits
+            Simulants.TitlePlay.ClickEvent => ShowGameplay
+            Simulants.TitleExit.ClickEvent => Exit
+            Simulants.CreditsBack.ClickEvent => ShowTitle
+            Simulants.Gameplay.QuitEvent => ShowTitle]
+
+    // here we handle the above messages
+    override this.Message (_, message, _, _) =
+        match message with
+        | ShowTitle -> just Title
+        | ShowCredits -> just Credits
+        | ShowGameplay -> just Gameplay
+
+    // here we handle the above commands
+    override this.Command (_, command, _, world) =
+        match command with
+        | Exit ->
+            if world.Unaccompanied
+            then just (World.exit world)
+            else just world
+
+    // here we describe the content of the game, including all of its screens.
+    override this.Content (_, _) =
+        [Content.screen Simulants.Splash.Name (Slide (Constants.Dissolve.Default, Constants.Slide.Default, None, Simulants.Title)) [] []
+         Content.screenWithGroupFromFile Simulants.Title.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Title.nugroup" [] []
+         Content.screenWithGroupFromFile Simulants.Credits.Name (Dissolve (Constants.Dissolve.Default, None)) "Assets/Gui/Credits.nugroup" [] []
+         Content.screen<GameplayDispatcher> Simulants.Gameplay.Name (Dissolve (Constants.Dissolve.Default, None)) [] []]
