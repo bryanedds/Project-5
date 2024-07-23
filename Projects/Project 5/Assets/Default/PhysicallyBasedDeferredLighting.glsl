77b2d0a7640fbc07ac186d9af5c7dbde402291a3
bryanedds <bryanedds@gmail.com>
2024-07-23 04:40:28 -0400

    Added distance filter.

--- Projects/Project 5/Assets/Default/PhysicallyBasedDeferredLighting.glsl
+++ Projects/Project 5/Assets/Default/PhysicallyBasedDeferredLighting.glsl
@@ -354,6 +354,7 @@ void main()
                 specularAvg * // filter out as specularity descreases
                 (1.0 - surfaceSlope) * // filter out as slope increases
                 (1.0 - max(dot(-positionViewNormal, reflectionView), 0.0)) * // filter out as reflection angles toward eye
+                (1.0 - clamp(length(currentPositionView - positionView) / reflectionDistanceMax, 0, 1)) * // filter out as reflection point reaches max distance
                 (currentUV.x >= 0.0 && currentUV.x <= 1.0 ? 1.0 : 0.0) * // ensure reflection in uv range
                 (currentUV.y >= 0.0 && currentUV.y <= 1.0 ? 1.0 : 0.0); // ensure reflection in uv range
             visibility = clamp(visibility, 0.0, 1.0);
