5b571aaf69afa67a383b77c4318d0e5527849776
bryanedds <bryanedds@gmail.com>
2024-09-27 03:14:19 -0400

    Got navigable room up and running.

--- Projects/Project 5/Assets.fs
+++ Projects/Project 5/Assets.fs
@@ -20,3 +20,4 @@ module Assets =
     module Gameplay =
 
         let PackageName = "Gameplay"
+        let Sophie = asset<AnimatedModel> PackageName "Sophie"
--- Projects/Project 5/Gameplay.fs
+++ Projects/Project 5/Gameplay.fs
@@ -48,9 +48,42 @@ type GameplayDispatcher () =
         // declare scene group when selected
         let world =
             if screen.GetSelected world then
+
+                // begin scene declaration
                 let world = World.beginGroupFromFile "Scene" "Assets/Gameplay/Scene.nugroup" [] world
-                let rotation = Quaternion.CreateFromAxisAngle ((v3 1.0f 0.75f 0.5f).Normalized, gameplay.GameplayTime % 360L |> single |> Math.DegreesToRadians)
-                let world = World.doStaticModel "StaticModel" [Entity.Position .= v3 0.0f 0.0f -2.0f; Entity.Rotation @= rotation] world
+
+                // declare player
+                let (_, world) = World.doCharacter3d "Player" [Entity.AnimatedModel .= Assets.Gameplay.Sophie; Entity.Position .= v3 1.0f 0.0f -1.0f] world
+
+                // move player
+                let player = world.RecentEntity
+                let playerBodyId = player.GetBodyId world
+                let playerSpeed = 1.8f * world.GameDelta.Seconds
+                let playerRotation = player.GetRotation world
+                let playerVelocity =
+                    (if World.isKeyboardKeyDown KeyboardKey.W world then playerRotation.Forward * playerSpeed else v3Zero) +
+                    (if World.isKeyboardKeyDown KeyboardKey.S world then playerRotation.Back * playerSpeed else v3Zero) +
+                    (if World.isKeyboardKeyDown KeyboardKey.A world then playerRotation.Left * playerSpeed else v3Zero) +
+                    (if World.isKeyboardKeyDown KeyboardKey.D world then playerRotation.Right * playerSpeed else v3Zero)
+                let grounded = World.getBodyGrounded playerBodyId world
+                let turnSpeed = 1.8f * world.GameDelta.Seconds * if grounded then 1.0f else 0.75f
+                let turnVelocity =
+                    (if World.isKeyboardKeyDown KeyboardKey.Left world then turnSpeed else 0.0f) +
+                    (if World.isKeyboardKeyDown KeyboardKey.Right world then -turnSpeed else 0.0f)
+                let world = player.SetPosition (player.GetPosition world + playerVelocity) world
+                let world = player.SetRotation (player.GetRotation world * Quaternion.CreateFromAxisAngle (v3Up, turnVelocity)) world
+
+                // update eye to look at player
+                let world =
+                    if world.Advancing then
+                        let position = player.GetPosition world
+                        let rotation = player.GetRotation world
+                        let world = World.setEye3dCenter (position + v3Up * 1.75f - rotation.Forward * 2.0f) world
+                        let world = World.setEye3dRotation rotation world
+                        world
+                    else world
+
+                // end scene declaration
                 World.endGroup world
             else world
 
