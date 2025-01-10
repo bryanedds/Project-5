4851920db62f9488624713c900d749984f1da9fa
bryanedds <bryanedds@gmail.com>
2025-01-09 20:27:30 -0500

    Started work on engine upgrade.

--- Projects/Project 5/Gameplay.fs
+++ Projects/Project 5/Gameplay.fs
@@ -27,58 +27,52 @@ type GameplayDispatcher () =
     // here we define the behavior of our gameplay
     override this.Process (gameplay, world) =
 
-        // declare entities only when selected
-        if gameplay.GetSelected world then
+        // begin scene declaration
+        let world = World.beginGroupFromFile "Scene" "Assets/Gameplay/Scene.nugroup" [] world
 
-            // begin scene declaration
-            let world = World.beginGroupFromFile "Scene" "Assets/Gameplay/Scene.nugroup" [] world
+        // declare player
+        let (playerBodyId, _, world) =
+            World.doCharacter3d "Player"
+                [Entity.Position .= v3 1.0f 0.0f -1.0f
+                 Entity.Size .= v3 1.5f 2.0f 1.5f
+                 Entity.Offset .= v3 0.0f 1.0f 0.0f
+                 Entity.AnimatedModel .= Assets.Gameplay.Sophie] world
+        let player = world.RecentEntity
 
-            // declare player
-            let (playerBodyId, _, world) =
-                World.doCharacter3d "Player"
-                    [Entity.Position .= v3 1.0f 0.0f -1.0f
-                     Entity.Size .= v3 1.5f 2.0f 1.5f
-                     Entity.Offset .= v3 0.0f 1.0f 0.0f
-                     Entity.AnimatedModel .= Assets.Gameplay.Sophie] world
-            let player = world.RecentEntity
+        // move player
+        let playerSpeed = 1.8f * world.GameDelta.Seconds
+        let playerRotation = player.GetRotation world
+        let playerVelocity =
+            (if World.isKeyboardKeyDown KeyboardKey.W world then playerRotation.Forward * playerSpeed else v3Zero) +
+            (if World.isKeyboardKeyDown KeyboardKey.S world then playerRotation.Back * playerSpeed else v3Zero) +
+            (if World.isKeyboardKeyDown KeyboardKey.A world then playerRotation.Left * playerSpeed else v3Zero) +
+            (if World.isKeyboardKeyDown KeyboardKey.D world then playerRotation.Right * playerSpeed else v3Zero)
+        let grounded = World.getBodyGrounded playerBodyId world
+        let turnSpeed = 1.8f * world.GameDelta.Seconds * if grounded then 1.0f else 0.75f
+        let turnVelocity =
+            (if World.isKeyboardKeyDown KeyboardKey.Left world then turnSpeed else 0.0f) +
+            (if World.isKeyboardKeyDown KeyboardKey.Right world then -turnSpeed else 0.0f)
+        let world = player.SetPosition (player.GetPosition world + playerVelocity) world
+        let world = player.SetRotation (player.GetRotation world * Quaternion.CreateFromAxisAngle (v3Up, turnVelocity)) world
 
-            // move player
-            let playerSpeed = 1.8f * world.GameDelta.Seconds
-            let playerRotation = player.GetRotation world
-            let playerVelocity =
-                (if World.isKeyboardKeyDown KeyboardKey.W world then playerRotation.Forward * playerSpeed else v3Zero) +
-                (if World.isKeyboardKeyDown KeyboardKey.S world then playerRotation.Back * playerSpeed else v3Zero) +
-                (if World.isKeyboardKeyDown KeyboardKey.A world then playerRotation.Left * playerSpeed else v3Zero) +
-                (if World.isKeyboardKeyDown KeyboardKey.D world then playerRotation.Right * playerSpeed else v3Zero)
-            let grounded = World.getBodyGrounded playerBodyId world
-            let turnSpeed = 1.8f * world.GameDelta.Seconds * if grounded then 1.0f else 0.75f
-            let turnVelocity =
-                (if World.isKeyboardKeyDown KeyboardKey.Left world then turnSpeed else 0.0f) +
-                (if World.isKeyboardKeyDown KeyboardKey.Right world then -turnSpeed else 0.0f)
-            let world = player.SetPosition (player.GetPosition world + playerVelocity) world
-            let world = player.SetRotation (player.GetRotation world * Quaternion.CreateFromAxisAngle (v3Up, turnVelocity)) world
+        // update eye to look at player
+        let world =
+            if world.Advancing then
+                let position = player.GetPosition world
+                let rotation = player.GetRotation world
+                let world = World.setEye3dCenter (position + v3Up * 1.8f - rotation.Forward * 1.0f + rotation.Right * 0.25f) world
+                let world = World.setEye3dRotation rotation world
+                world
+            else world
 
-            // update eye to look at player
-            let world =
-                if world.Advancing then
-                    let position = player.GetPosition world
-                    let rotation = player.GetRotation world
-                    let world = World.setEye3dCenter (position + v3Up * 1.8f - rotation.Forward * 1.0f + rotation.Right * 0.25f) world
-                    let world = World.setEye3dRotation rotation world
-                    world
-                else world
+        // end scene declaration
+        let world = World.endGroup world
 
-            // end scene declaration
-            let world = World.endGroup world
-
-            // declare gui group
-            let world = World.beginGroup "Gui" [] world
-            let (clicked, world) = World.doButton "Quit" [Entity.Text .= "Quit"; Entity.Position .= v3 232.0f -144.0f 0.0f] world
-            let world = if clicked then gameplay.SetGameplayState Quit world else world
-            World.endGroup world
-
-        // otherwise nothing to do
-        else world
+        // declare gui group
+        let world = World.beginGroup "Gui" [] world
+        let (clicked, world) = World.doButton "Quit" [Entity.Text .= "Quit"; Entity.Position .= v3 232.0f -144.0f 0.0f] world
+        let world = if clicked then gameplay.SetGameplayState Quit world else world
+        World.endGroup world
 
     // this is a semantic fix-up that allows the editor to avoid creating an unused group. This is specific to the
     // ImNui API that is needed to patch a little semantic hole inherent in the immediate-mode programming idiom.
