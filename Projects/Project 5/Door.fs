52ed7cbeac8c21a91fdb93641265a9e87929d32d
bryanedds <bryanedds@gmail.com>
2025-03-02 10:46:38 -0500

    More work on doors.

--- Projects/Project 5/Door.fs
+++ Projects/Project 5/Door.fs
@@ -11,6 +11,9 @@ module DoorDispatcherExtensions =
         member this.GetDoorState world : DoorState = this.Get (nameof this.DoorState) world
         member this.SetDoorState (value : DoorState) world = this.Set (nameof this.DoorState) value world
         member this.DoorState = lens (nameof this.DoorState) this this.GetDoorState this.SetDoorState
+        member this.GetRotationInitial world : Quaternion = this.Get (nameof this.RotationInitial) world
+        member this.SetRotationInitial (value : Quaternion) world = this.Set (nameof this.RotationInitial) value world
+        member this.RotationInitial = lens (nameof this.RotationInitial) this this.GetRotationInitial this.SetRotationInitial
 
 type DoorDispatcher () =
     inherit Entity3dDispatcherImNui (false, false, false)
@@ -20,21 +23,29 @@ type DoorDispatcher () =
 
     static member Properties =
         [define Entity.Sensor true
-         define Entity.DoorState DoorClosed]
+         define Entity.DoorState DoorClosed
+         define Entity.RotationInitial quatIdentity]
 
     override this.PresenceOverride =
         ValueSome Exterior
 
+    override this.Register (entity, world) =
+        match entity.Parent with
+        | :? Entity as parent -> entity.SetRotationInitial (parent.GetRotationLocal world) world
+        | _ -> Log.warn "Door parent not an entity as intended."; world
+
     override this.Process (entity, world) =
-        match entity.GetDoorState world with
-        | DoorClosed -> world
-        | DoorClosing startTime ->
-            let progress = 1.0f - GameTime.progress startTime world.GameTime 1.25f
-            (entity.Parent :?> Entity).SetRotationLocal (Quaternion.CreateFromAxisAngle (v3Up, -progress * 2.0f)) world
-        | DoorOpened -> world
-        | DoorOpening startTime ->
-            let progress = GameTime.progress startTime world.GameTime 1.25f
-            (entity.Parent :?> Entity).SetRotationLocal (Quaternion.CreateFromAxisAngle (v3Up, -progress * 2.0f)) world
+        match entity.Parent with
+        | :? Entity as parent -> 
+            let rotationInitial = entity.GetRotationInitial world
+            let progress =
+                match entity.GetDoorState world with
+                | DoorClosed -> 0.0f
+                | DoorClosing startTime -> 1.0f - GameTime.progress startTime world.GameTime 1.25f
+                | DoorOpened -> 1.0f
+                | DoorOpening startTime -> GameTime.progress startTime world.GameTime 1.25f
+            parent.SetRotationLocal (Quaternion.CreateFromAxisAngle (v3Up, -progress * 2.0f) * rotationInitial) world
+        | _ -> world
 
     override this.GetAttributesInferred (_, _) =
         AttributesInferred.important (v3Dup 0.25f) v3Zero
