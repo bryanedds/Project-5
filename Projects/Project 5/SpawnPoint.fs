8fde135835ff579bf55207fb405b20c837dba59d
bryanedds <bryanedds@gmail.com>
2025-03-19 16:56:24 -0400

    Removed unneeded ray cast overrides.

--- Projects/Project 5/SpawnPoint.fs
+++ Projects/Project 5/SpawnPoint.fs
@@ -8,10 +8,5 @@ open MyGame
 type SpawnPointDispatcher () =
     inherit Entity3dDispatcherImNui (false, false, false)
 
-    override this.RayCast (ray, entity, world) =
-        let intersectionOpt = ray.Intersects (entity.GetBounds world)
-        if intersectionOpt.HasValue then [|intersectionOpt.Value|]
-        else [||]
-
     override this.GetAttributesInferred (_, _) =
         AttributesInferred.important (v3Dup 0.25f) v3Zero
--- Projects/Project 5/WayPoint.fs
+++ Projects/Project 5/WayPoint.fs
@@ -9,9 +9,7 @@ type WayPointDispatcher () =
     inherit Entity3dDispatcherImNui (false, false, false)
 
     override this.RayCast (ray, entity, world) =
-        let intersectionOpt = ray.Intersects (entity.GetBounds world)
-        if intersectionOpt.HasValue then [|intersectionOpt.Value|]
-        else [||]
+        base.RayCast (ray, entity, world)
 
     override this.GetAttributesInferred (_, _) =
         AttributesInferred.important (v3Dup 0.25f) v3Zero
