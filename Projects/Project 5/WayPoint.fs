1a5d93db57a037d2c1bbba444d5f3ab67d374f91
bryanedds <bryanedds@gmail.com>
2025-03-21 19:36:30 -0400

    Code clean-up.

--- Projects/Project 5/WayPoint.fs
+++ Projects/Project 5/WayPoint.fs
@@ -8,8 +8,5 @@ open MyGame
 type WayPointDispatcher () =
     inherit Entity3dDispatcherImNui (false, false, false)
 
-    override this.RayCast (ray, entity, world) =
-        base.RayCast (ray, entity, world)
-
     override this.GetAttributesInferred (_, _) =
         AttributesInferred.important (v3Dup 0.25f) v3Zero
