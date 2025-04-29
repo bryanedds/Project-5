163b6bfcb0e29dc5fcedcb43aeb5ce523f44ab63
bryanedds <bryanedds@gmail.com>
2025-04-28 22:31:32 -0400

    Played with construction of placement types.

--- Projects/Project 5/Data.fs
+++ Projects/Project 5/Data.fs
@@ -300,3 +300,154 @@ and CharacterType =
         | Hunter -> HunterState HunterState.initial
         | Stalker -> StalkerState StalkerState.initial
         | Player -> PlayerState PlayerState.initial
+
+(* Prelude *)
+
+type BooleanOperation =
+    | BooleanUnion
+    | BooleanDifference
+    | BooleanExclusive
+    | BooleanIntersection
+
+type DiscreteOperation =
+    | DiscreteOperation of (int -> Vector3 (*position*) -> Vector3 (*normal*) -> Matrix4x4 -> World -> World)
+    | CreateEntity of EntityDescriptor
+    | Nop
+
+type 'a Bounded =
+    | Included of 'a
+    | Excluded
+
+type DiscreteMapping = int -> DiscreteOperation Bounded
+
+(* Discrete Space *)
+
+type DiscreteSpace =
+    | DiscreteSpace of ((Vector3 -> bool) * (unit -> Vector3 array))
+    | Points of Vector3 array
+
+type DiscreteDiscretize =
+    | DiscreteDiscretize of (DiscreteSpace -> DiscreteMapping)
+    | DiscreteIdentity // trivial identity mapping
+
+type DiscreteUnop =
+    | DiscreteUnop of (DiscreteSpace -> DiscreteSpace)
+    | DiscreteTransformation of Affine
+
+type DiscreteBinop =
+    | DiscreteBinop of (DiscreteSpace -> DiscreteSpace -> DiscreteSpace)
+    | DiscreteBooleanOperation of BooleanOperation
+
+(* Linear Space *)
+
+type LinearSpace =
+    | LinearSpace of ((Vector3 -> bool) * (unit -> Vector3 array))
+    | Spline of Vector3 array
+
+type LinearDiscretize =
+    | LinearDiscretize of (LinearSpace -> DiscreteMapping)
+    | LinearSegmentation of int
+
+type LinearUnop =
+    | LinearUnop of (LinearSpace -> LinearSpace)
+    | LinearTransformation of Affine
+
+type LinearBinop =
+    | LinearBinop of (LinearSpace -> LinearSpace -> LinearSpace)
+    | LinearConcat // concatenates two linear spaces
+
+(* Euclidean Space *)
+
+type EuclideanSpace =
+    | EuclideanSpace of ((Vector3 -> bool) * (unit -> Vector3 array))
+    | EuclideanBox of Box3
+    | EuclideanPlane of Plane3
+    | EuclideanSphere of Sphere
+
+type EuclideanDiscretize =
+    | EuclideanDiscretize of (EuclideanSpace -> DiscreteMapping)
+    | EuclideanGrid of Vector3
+
+type EuclideanUnop =
+    | EuclideanUnop of (EuclideanSpace -> EuclideanSpace)
+    | EuclideanTransformation of Affine
+
+type EuclideanBinop =
+    | EuclideanBinop of (EuclideanSpace -> EuclideanSpace -> EuclideanSpace)
+    | EuclideanBooleanOperation of BooleanOperation
+
+(* Topography Space *)
+
+type TopographySpace =
+    | TopographySpace of ((Vector3 -> bool) * (unit -> Vector3 array))
+    | HeightMap of HeightMap * Matrix4x4
+    | StaticModel of StaticModel AssetTag * Matrix4x4
+    | StaticModelSurface of int * StaticModel AssetTag * Matrix4x4
+    | StaticModelEntity of Entity
+    | StaticModelSurfaceEntity of Entity
+    | RigidBodyEntity of Entity
+
+type TopographyDiscretize =
+    | TopographyDiscretize of (TopographySpace -> DiscreteMapping)
+    | TopographyBarycentric of Vector2
+    | Centers // take center of triangles, including for height field triangles
+    | Points // take unique points of triangles, including for height field triangles
+
+type TopographyUnop =
+    | TopographyUnop of (TopographySpace -> TopographySpace)
+    | TopographyTransformation of Affine
+
+type TopographyBinop =
+    | TopographyBinop of (TopographySpace -> TopographySpace -> TopographySpace)
+    | TopographyConcat // concatenates two topography spaces
+
+type TopographyToEuclidean = TopographySpace -> EuclideanSpace
+
+type Space =
+    | DiscreteSpace of DiscreteSpace
+    | LinearSpace of LinearSpace
+    | EuclideanSpace of EuclideanSpace
+    | TopographySpace of TopographySpace
+    | Spaces of Space array
+
+type SpaceBinop =
+    | SpaceBinop of (Space -> Space -> Space)
+    | Stamp of (DiscreteSpace -> Space -> Space) // duplicates a space at every discrete point in some way
+    | StampAffine of Affine // duplicates a space at every discrete point with the given affine transformation
+    | StampIdentity // duplicates a space at every discrete point with identity transformation
+    | Branch of (LinearSpace -> Space -> Space) // duplicates a space at every linear segment in some way
+    | BranchAffine of Affine
+    | BranchIdentity
+
+(* Placement *)
+
+type PlacementObject = string // like "Desk" or "Apple" or "Book" or "Utensil"
+
+type PlacementPlatform =
+    | PlacementPlatform of ((*Normal*) Vector3 -> bool)
+    | TopUp of AllowOverhang : bool // like on top of a box
+    | BottomUp of ExcludingSides : bool // like inside a box
+    | Anywhere
+    | Nowhere
+
+type PlacementAttribute =
+    | ConvexPlatform of Vector3 array * PlacementPlatform // uses triangle list
+    | BoxPlatform of Box3 * PlacementPlatform
+    | QuadPlatform of Vector3 * Vector3 * Vector3 * Vector3 * PlacementPlatform
+    | BoundsPlatform of PlacementPlatform // uses existing spatial bounds rather than user-defined bounds
+
+type PlacementWeight =
+    | Noise of (Vector3 -> single)
+    | Perlin of Scale : single
+    | WeightImage of Scale : single * Image AssetTag
+
+type PlacementRule =
+    | Stack of PlacementObject * PlacementObject // can set right on left
+
+type PlacementDistribution =
+    | PlacementDistribution of PlacementObject * PlacementWeight
+
+type PlacementDefinitions =
+    { Rules : PlacementRule array
+      Attributes : Map<PlacementObject, PlacementAttribute array>
+      Distributions : PlacementDistribution array }
